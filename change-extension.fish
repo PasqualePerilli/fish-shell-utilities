67ae9faf951b847c3b2fc862a1366081bc46bb8d
Pasquale <82742942+PasqualePerilli@users.noreply.github.com>
2023-03-31 04:10:14 -0500

    Add files via upload

---
+++ change-extension.fish
@@ -0,0 +1,46 @@
+function change-extension
+	set -l array_length (echo $argv | wc -w)
+	set -l input_file ''
+	set -l new_extension ''
+	set -l output_file
+	set -l next_index 1
+	if test $array_length -gt 0
+		for index in (seq 1 $array_length)
+			set next_index (math  $next_index + 1)
+			switch "$argv[$index]"
+				case -i --input
+					set input_file $argv[$next_index]
+				case -e --extension
+					set new_extension $argv[$next_index]
+				case -h --help
+					echo 'Use the change-extension command as follows'
+					echo 'change-extension --input /path/to/file.pdf --extension doc'
+					echo 'Or alternatively:'
+					echo 'change-extension -i /path/to/file.pdf -e doc'
+			end
+		end
+		if test $input_file != ""
+			if test $new_extension != ""
+				set output_file (string trim (echo $input_file | sed 's/\.[^.]*$//'))
+				set output_file "$output_file.$new_extension"
+				echo $output_file
+			else
+				echo 'Use the change-extension command as follows'
+				echo 'change-extension --input /path/to/file.pdf --extension doc'
+				echo 'Or alternatively:'
+				echo 'change-extension -i /path/to/file.pdf -e doc'
+			end
+		else
+			echo 'Use the change-extension command as follows'
+			echo 'change-extension --input /path/to/file.pdf --extension doc'
+			echo 'Or alternatively:'
+			echo 'change-extension -i /path/to/file.pdf -e doc'
+		end
+	else
+		echo 'Use the change-extension command as follows'
+		echo 'change-extension --input /path/to/file.pdf --extension doc'
+		echo 'Or alternatively:'
+		echo 'change-extension -i /path/to/file.pdf -e doc'
+	end
+
+end
