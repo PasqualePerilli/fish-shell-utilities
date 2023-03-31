178a03b89be24f616e99d670754c2931a19ba422
Pasquale <82742942+PasqualePerilli@users.noreply.github.com>
2023-03-31 05:46:17 -0500

    Add files via upload

--- convert-mp3.fish
+++ convert-mp3.fish
@@ -2,15 +2,15 @@ function convert-mp3
 	set -l array_length (echo $argv | wc -w)
 	set -l input_file ''
 	set -l output_file ''
-	echo 'There are ' $array_length ' parameters'
+	#echo 'There are ' $array_length ' parameters'
 	if test $array_length -gt 0
 		set input_file (string trim $argv[1])
 		set output_file (string trim (echo $input_file | sed 's/\.[^.]*$//'))
 		set output_file "$output_file.mp3"
-		echo 'Input file is ['$input_file']'
-		echo 'Output file is ['$output_file']'
-		echo 'ffmpeg -i ' '"'$input_file'"' ' -vn -ar 44100 -ac 2 -b:a 192k' '"'$output_file'"'
-		ffmpeg -i "$input_file" -vn -ar 44100 -ac 2 -b:a 192k "$output_file"
+		#echo 'Input file is ['$input_file']'
+		#echo 'Output file is ['$output_file']'
+		#echo 'ffmpeg -i ' '"'$input_file'"' ' -vn -ar 44100 -ac 2 -b:a 192k' '"'$output_file'"'
+		ffmpeg -i "$input_file" -vn -ar 44100 -ac 2 -b:a 192k "$output_file" -hide_banner -loglevel error
 		echo 'Conversion complete! Saved ['$output_file']'
 	else
 		echo 'Please re-run this command and add the input file location, such as: convert-mp3 /home/$USER/video.mp4'
